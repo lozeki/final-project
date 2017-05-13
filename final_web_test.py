115c3913fb18435da0c8483c231dc69c1ba58f49
lozeki <minhtri.ng@gmail.com>
2017-05-13 00:59:06 -0400

    fix the test_poses function

--- final_web_test.py
+++ final_web_test.py
@@ -23,7 +23,7 @@ class FlaskrTestCase(unittest.TestCase):
         # Replace UNH698 Website with the text you expect to see on you topi$
         assert b'YOGA PRACTICE' in rv.data 
 
- def test_poses(self):
+    def test_poses(self):
         # Replace '/' with the page path you want to make
         rv = self.app.get('/poses')  
         # Replace UNH698 Website with the text you expect to see on you topi$
